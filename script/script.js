4f8a582f0486589f07007e244105f6b625311ce1
Filip Czech <czechfilip901@gmail.com>
2022-12-19 17:03:52 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    
+    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+});
+
+
